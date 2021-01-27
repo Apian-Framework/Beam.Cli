ab34663d5f6cd2b21cf3d7b0e9d6111e61ce9e94
jim berry <jim@bigfattail.com>
2021-01-27 15:28:30 -0500

    Mostly CLI arg handling tests

--- tests/BeamCli.Tests/BeamCli.Tests.cs
+++ tests/BeamCli.Tests/BeamCli.Tests.cs
@@ -7,11 +7,14 @@ using Newtonsoft.Json;
 using Moq;
 using Moq.Protected;
 using BeamCli;
+using UniLog;
 using BeamGameCode;
+using System.Reflection;
 
 namespace BeamCliTests
 {
     [TestFixture]
+
 
     public class CliDriverTests
     {
@@ -45,7 +48,55 @@ namespace BeamCliTests
             Assert.That(drvw.bgn, Is.InstanceOf<BeamGameNet>());
             Assert.That(drvw.appl, Is.InstanceOf<BeamApplication>());
         }
+    }
 
+    [TestFixture]
+
+    public class CliProgramTests
+    {
+        public object SettingByName(BeamUserSettings s, string name )
+        {
+           Type myType = typeof(BeamUserSettings);
+           FieldInfo fi = myType.GetField(name);
+           return fi.GetValue(s);
+        }
+
+        public class WrappedProgram : Program
+        {
+            public static BeamUserSettings DoGetSettings(string[] args)
+            {
+                return GetSettings(args);
+            }
+        }
+
+
+
+        [TestCase("--startmode,2", "startMode", 2)]
+        [TestCase("--bikectrl,ai", "localPlayerCtrlType", "ai")]
+        [TestCase("--defloglvl,info", "defaultLogLevel", "info")]
+        [TestCase("--throwonerror,true", "DefaultThrowOnError", true)]
+        [TestCase("--gamespec,foo/bar+", "GameSpec", "foo/bar+")]
+
+        public void CliProgram_GetSettings(string argsString, string settingName, object val)
+        {
+            var args = argsString.Split(',');
+            BeamUserSettings sets = WrappedProgram.DoGetSettings(args);
+            Assert.That(sets.version, Is.EqualTo(UserSettingsMgr.currentVersion));
+
+            switch(settingName)
+            {
+            case "GameSpec":
+                Assert.That(sets.tempSettings["gameSpec"], Is.EqualTo(val));
+                break;
+            case "DefaultThrowOnError":
+                Assert.That(UniLogger.DefaultThrowOnError, Is.EqualTo(val));
+                break;
+            default:
+                Assert.That(SettingByName(sets, settingName), Is.EqualTo(val));
+                break;
+            }
+
+        }
 
     }
 
