e6fec3b92ad1f0396f97644a483fa1013aa6c03c
jim berry <jim@bigfattail.com>
2021-01-27 12:47:20 -0500

    Sone CliDriver tests

--- tests/BeamCli.Tests/BeamCli.Tests.cs
+++ tests/BeamCli.Tests/BeamCli.Tests.cs
@@ -5,20 +5,48 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Newtonsoft.Json;
 using Moq;
+using Moq.Protected;
 using BeamCli;
+using BeamGameCode;
 
 namespace BeamCliTests
 {
     [TestFixture]
+
     public class CliDriverTests
     {
+        public class CliDriverWrapper : CliDriver
+        {
+            public void CallInit(BeamUserSettings bus )
+            {
+                base.Init(bus);
+            }
+        }
+
         [Test]
         public void CliDriver_ConstructorWorks()
         {
             CliDriver drv = new CliDriver();
+            long tfms = drv.targetFrameMs;
+
             Assert.That(drv, Is.Not.Null);
-            Assert.That(drv.targetFrameMs, Is.EqualTo(16)); // default val
+            Assert.That(tfms, Is.EqualTo(16)); // default val
         }
+
+        [Test]
+        public void CliDriver_Init()
+        {
+            Mock<BeamUserSettings> settings = new Mock<BeamUserSettings>();
+
+            CliDriverWrapper drvw = new CliDriverWrapper();
+            drvw.CallInit(settings.Object);
+
+            Assert.That(drvw.fe, Is.InstanceOf<BeamCliFrontend>());
+            Assert.That(drvw.bgn, Is.InstanceOf<BeamGameNet>());
+            Assert.That(drvw.appl, Is.InstanceOf<BeamApplication>());
+        }
+
+
     }
 
 }
